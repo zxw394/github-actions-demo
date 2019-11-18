3e94a66378f7d8b8f64a4267976c492749b57fd5
zxw394 <zhangxwforload@126.com>
2019-11-18 02:54:25 +0000

    Deploying to master from master c16f18055a2cce573cc0e7a51dc607556568c829

---
+++ precache-manifest.06969632c13e973a66f41e1902cf1c05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d2c56fa2ea5af2fc953d775af97c053",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "52a56279468d00e8b04c",
+    "url": "/github-actions-demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "fcdbb97075feda89d7f3",
+    "url": "/github-actions-demo/static/js/2.ad237ff1.chunk.js"
+  },
+  {
+    "revision": "52a56279468d00e8b04c",
+    "url": "/github-actions-demo/static/js/main.750ceaab.chunk.js"
+  },
+  {
+    "revision": "c276d83db910dc11e2cc",
+    "url": "/github-actions-demo/static/js/runtime-main.f2796007.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/github-actions-demo/static/media/logo.25bf045c.svg"
+  }
+]);
